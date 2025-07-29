461ecd0cc13c88133d70987875c48bf0cfcbdc68
jrx100th <jenith.ch15@gmail.com>
2025-07-29 17:33:57 +0530

    Day 34
    Made the Quiz GUI
    by the way
    age : int 
    
    so here now age should only be int
    similar to functions
    def func1(age : int)
    will only take int
    
    now return type
    def funct2(): -> bool
    should only return bool, when we return from the function.

---
+++ 100 days, day 34/quizzler-app-start/ui.py
@@ -0,0 +1,62 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
+THEME_COLOR = "#375362"
+
+class QuizInterface:
+    def __init__(self,quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(bg=THEME_COLOR, padx=20, pady=20)
+
+        self.score = Label(text= self.quiz.score,bg=THEME_COLOR,fg="white")
+        self.score.grid(row=0,column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            150,
+            width=280,
+            text="Question Text", 
+            fill=THEME_COLOR, 
+            font=("Arial",20,"italic")
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_img = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_img, highlightthickness=0, height=80, width=80, command=self.true_pressed)
+        self.true_button.grid(row=2, column=0)
+
+        false_img = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_img, highlightthickness=0, height=80, width=80,command=self.false_pressed)
+        self.false_button.grid(row=2, column=1)
+
+        self.get_next_question()
+
+        self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score.config(text=f"Score : {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text=f"You've reached the end of the quiz.\nScore {self.quiz.score}")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def false_pressed(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
